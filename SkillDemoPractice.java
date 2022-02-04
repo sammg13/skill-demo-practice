d6c2fa2e7ab87cf5d4fda70c99caedfc446b0f47
sammg13 <s3gonzal@ucsd.edu>
2022-02-04 10:14:08 -0800

    libs and starter code

--- SkillDemoPractice.java
+++ SkillDemoPractice.java
@@ -1,3 +1,7 @@
 public class SkillDemoPractice {
   // will use vs code
+  
+  public static int return13(){
+    return 1;
+  }
 }
---
+++ SkillDemoPracticeTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoPracticeTest {
+    @Test
+    public void return13(){
+        assertEquals(13, SkillDemoPractice.return13()); // will fail
+    }
+}
