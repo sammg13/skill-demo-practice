a67c263eeeb7306495d9effc81e5c4a006407556
sammg13 <s3gonzal@ucsd.edu>
2022-02-09 19:16:21 -0800

    Update SkillDemoPracticeTest.java

--- SkillDemoPracticeTest.java
+++ SkillDemoPracticeTest.java
@@ -7,5 +7,5 @@ public class SkillDemoPracticeTest {
         assertEquals(13, SkillDemoPractice.return13()); // will fail
     }
 }
-// javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar SkillDemoPracticeTest.java
+// javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar SkillDemoPractice.java SkillDemoPracticeTest.java
 // java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore SkillDemoPracticeTest
